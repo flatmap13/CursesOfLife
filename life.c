fe74c71c6fca26d6f161007f2bc252feae39f3b3
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-03-17 15:39:49 +0100

    replaced defines with static variables

--- life.c
+++ life.c
@@ -12,8 +12,9 @@ int main(void)
 	start_color();
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
 	init_pair(2, COLOR_GREEN, COLOR_BLUE);
+	init_params();
 
-	bool world[NUM_CELLS];
+	bool world[_num_cells];
 	init_world(world);
 	int ch;
 	while((ch = getch()) != 'q') {
@@ -33,54 +34,61 @@ int main(void)
 	}
 
 	clrtoeol();
-	refresh();
 	endwin();
 
 	return 0;
 }
 
+void init_params(void)
+{
+	getmaxyx(stdscr, _height, _width);
+	_height -= 2;
+	_num_cells = _height * _width;
+}
+
 void show_info(const bool *world)
 {
 	int num_alive = 0, n;
-	for(n = 0; n < NUM_CELLS; n++)
-		num_alive = world[n] ? num_alive + 1 : num_alive;
+	for(n = 0; n < _num_cells; n++)
+		num_alive += world[n];
+
 	attron(COLOR_PAIR(2) | A_BOLD);
-	mvprintw(LINES - 1, 3, " Press q to exit ");
-	mvprintw(LINES - 1, (WIDTH - 18) / 2, " Cells alive: %d ", num_alive);
-	mvprintw(LINES - 1, WIDTH - 21, " Press r to reset ");
+	mvprintw(LINES - 1, 2, "  Press q to exit  ");
+	mvprintw(LINES - 1, (_width - 17) / 2, "  Cells alive: %d  ", num_alive);
+	mvprintw(LINES - 1, _width - 21, "  Press r to reset  ");
 	attroff(COLOR_PAIR(2) | A_BOLD);
 }
 
 void init_world(bool *world)
 {
 	int n;
-	for(n = 0; n < NUM_CELLS; n++)
+	for(n = 0; n < _num_cells; n++)
 		world[n] = rand() % 2;
 }
 
 void draw_world(const bool *world)
 {
 	char chars[2] = {' ', 'O'};
-	int y, x;
-	for(y = 0; y < HEIGHT; y++)
-		for(x = 0; x < WIDTH; x++)
-			mvprintw(y, x, "%c", chars[world[POS(x, y)]]);
+	int n;
+	move(0,0);
+	for(n = 0; n < _num_cells; n++)
+		printw("%c", chars[world[n]]);
 }
 
 void update_world(bool *world)
 {
-	bool new_world[NUM_CELLS];
+	bool new_world[_num_cells];
 	int y, x;
-	for(y = 0; y < HEIGHT; y++) {
-		for(x = 0; x < WIDTH; x++) {
+	for(y = 0; y < _height; y++) {
+		for(x = 0; x < _width; x++) {
 			int neighbours = count_neighbours(world, x, y);
-			new_world[POS(x, y)] = neighbours == 2 && world[POS(x, y)]
+			new_world[pos(x, y)] = neighbours == 2 && world[pos(x, y)]
 				|| neighbours == 3;
 		}
 	}
 
 	int n;
-	for(n = 0; n < NUM_CELLS; n++)
+	for(n = 0; n < _num_cells; n++)
 		world[n] = new_world[n];
 }
 
@@ -98,6 +106,10 @@ int count_neighbours(const bool *world, const int x, const int y)
 
 bool get_cell(const bool *world, const int x, const int y)
 {
-	return world[POS(x,y)];
+	return world[pos(x,y)];
 }
 
+int pos(int x, int y)
+{
+	return y * _width + x;
+}
--- life.h
+++ life.h
@@ -2,15 +2,16 @@
 #include <time.h>
 #include <ncurses.h>
 
-#define WIDTH		COLS
-#define HEIGHT		LINES - 2
-#define NUM_CELLS	WIDTH * HEIGHT
-#define POS(X,Y)	Y * COLS + X
-
+void init_params(void);
 void show_info(const bool *world);
 void init_world(bool *world);
 void draw_world(const bool *world);
 void update_world(bool *world);
 int count_neightbours(const bool *world, const int x, const int y);
 bool get_cell(const bool *world, const int x, const int y);
+int pos(int x, int y);
+
+static int _width;
+static int _height;
+static int _num_cells;
 
